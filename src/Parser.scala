fdffaedb3641af2723839282205e8bfe2bba0534
qmac <quinten.mcnamara@gmail.com>
2016-04-20 00:28:31 -0500

    something that kinda works i guess

--- src/Parser.scala
+++ src/Parser.scala
@@ -1,7 +1,15 @@
 import scala.util.parsing.combinator._
 
 object BFParser extends JavaTokenParsers {
-	def parse(in: java.io.Reader) = parseAll(prog, in).get
+	def parseFile(in: java.io.Reader) = parseAll(prog, in).get
+	def parseLine(line: String) = parseAll(prog, line).get
 
-	def prog: Parser[List[Any]] = rep("")
+	override val skipWhitespace = false
+
+	def tab: Parser[Any] = "	" ~ statement
+	def space: Parser[Any] = " " ~ statement
+	def lf: Parser[Any] = "\n"
+	def statement: Parser[Any] = tab | space | lf
+
+	def prog = rep(statement)
 }
