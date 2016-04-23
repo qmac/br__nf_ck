434e5e4e6c3b3e171b743e2a241275e4b14ec1fe
qmac <quinten.mcnamara@gmail.com>
2016-04-23 15:43:08 -0500

    spacing stuff

--- src/Parser.scala
+++ src/Parser.scala
@@ -1,17 +1,17 @@
 import scala.util.parsing.combinator._
 
 object BFParser extends JavaTokenParsers {
-	def parseFile(in: java.io.Reader) = parseAll(prog, in).get
-	def parseLine(line: String) = parseAll(prog, line).get
+    def parseFile(in: java.io.Reader) = parseAll(prog, in).get
+    def parseLine(line: String) = parseAll(prog, line).get
 
-  override val whiteSpace = """([^(\s\\/_><+-.,\[\])]*)+""".r
+    override val whiteSpace = """([^(\s\\/_><+-.,\[\])]*)+""".r
 
-	def tab: Parser[Any] = "	"
-	def space: Parser[Any] = " "
-	def lf: Parser[Any] = "\n"
-	def statement: Parser[Any] = tab | space | lf | giveaway
-	
-	def giveaway : Parser[Any] = "\\"
+    def tab: Parser[Any] = "\t"
+    def space: Parser[Any] = " "
+    def lf: Parser[Any] = "\n"
+    def statement: Parser[Any] = tab | space | lf | giveaway
+    
+    def giveaway : Parser[Any] = "\\"
 
-	def prog = rep(statement)
+    def prog = rep(statement)
 }
--- src/Test.scala
+++ src/Test.scala
@@ -2,8 +2,8 @@ import java.io.FileReader
 import java.io.FileInputStream
  
 object BFTest {
-  def main(args: Array[String]): Unit = {
-    val reader = new FileReader("TestAdd1.ws")
-    println(BFParser.parseFile(reader))
-  }
+    def main(args: Array[String]): Unit = {
+        val reader = new FileReader("TestAdd1.ws")
+        println(BFParser.parseFile(reader))
+    }
 }
