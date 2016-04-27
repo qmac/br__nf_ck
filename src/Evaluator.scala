0d63923b26da6b39022e4b2908117d1fa6cf1c63
Siddharth Kumar <siddharth.kumar@utexas.edu>
2016-04-27 01:44:57 -0500

    initial eval

--- src/Evaluator.scala
+++ src/Evaluator.scala
@@ -1,3 +1,58 @@
-object BFEvaluator {
+import scala.collection.mutable
+import scala.io.StdIn.{readLine,readInt}
 
+object Evaluator {
+    
+    val tapesize = 100
+    var tape:Array[Int] = new Array[Int](tapesize)
+    
+    
+    
+    // Data Pointer
+    var ptr = 0
+    
+    def eval(cmdlist:List[Any])
+    {
+        var actual : List[Any] = cmdlist
+        actual.foreach(exec(_))
+        
+        
+    }
+    
+    def exec(cmd:Any) = cmd match
+    {
+        case ">" => increment_ptr()
+        case "<" => decrement_ptr()
+        case "+" => tape(ptr) = tape(ptr) + 1
+        case "-" => tape(ptr) = tape(ptr) - 1
+        case "." => output(tape(ptr))
+        case "," => tape(ptr) = readInt()
+        case subprogram : List[Any] => eval(subprogram)
+    }
+    
+    def output(num:Int)
+    {
+        print(num.toChar)
+    }
+    
+    def increment_ptr() {
+        if (ptr != tapesize - 1) {
+            ptr = ptr + 1
+        }
+        else {
+            ptr = 0
+        }
+    }
+    
+    def decrement_ptr() {
+        if (ptr != 0)  {
+            ptr = ptr - 1
+        }
+        else {
+            ptr = tapesize
+        }
+    }
+    
+    
 }
+
--- src/Test.scala
+++ src/Test.scala
@@ -1,10 +1,14 @@
 import java.io.FileReader
 import java.io.FileInputStream
+
+
  
 object BFTest {
     def main(args: Array[String]): Unit = {
-        val reader = new FileReader("../test/TestPrint1.b_f")
-        println(BFParser.parseFile(reader))
-        // println(BFParser.parseLine("   \t\n"))
+        // val reader = new FileReader("../../test/TestPrint1.b_f")
+        // println(BFParser.parseFile(reader))
+        
+        var ls : List[Any] = BFParser.parseLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++>,<.>.").productIterator.toList
+        println(Evaluator.eval(ls))
     }
 }
