e13ef0c2832c737f0fc4460093c275949f6339b0
Eugene Ng <eugenen@utexas.edu>
2016-04-26 23:50:26 -0500

    eval stack commands

--- src/Evaluator.scala
+++ src/Evaluator.scala
@@ -1,13 +1,24 @@
 import scala.collection.mutable.Stack
 
 object BFEvaluator {
+	var stack = Stack[Int]()
+
     def evaluate(operations: Any) : Unit = {
         operations match {
             case command :: remainingCommands => execute(command)
             case Nil => ;
         }
     }
+    def push(n: Int) = stack.push(n)
+    def duplicate = stack.push(stack.top)
+    def swap = {
+    	val prevTop = stack.pop
+    	val prevNext = stack.pop
+    	stack.push(prevTop)
+    	stack.push(prevNext)
+    }
+    def discard = stack.pop
     def execute(command: Any) : Unit = {
-
+    	
     }
 }
