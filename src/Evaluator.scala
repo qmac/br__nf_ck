0920371aa47982a62b40d9386dc877bf5feb871c
qmac <quinten.mcnamara@gmail.com>
2016-04-26 23:02:09 -0500

    some boilerplate stuff

--- src/Evaluator.scala
+++ src/Evaluator.scala
@@ -1,3 +1,13 @@
-object BFEvaluator {
+import scala.collection.mutable.Stack
 
+object BFEvaluator {
+    def evaluate(operations: Any) : Unit = {
+        operations match {
+            case command :: remainingCommands => execute(command)
+            case Nil => ;
+        }
+    }
+    def execute(command: Any) : Unit = {
+
+    }
 }
--- src/Test.scala
+++ src/Test.scala
@@ -3,8 +3,11 @@ import java.io.FileInputStream
  
 object BFTest {
     def main(args: Array[String]): Unit = {
-        val reader = new FileReader("../test/TestPrint1.b_f")
-        println(BFParser.parseFile(reader))
+        val reader = new FileReader("../test/TestPrint1.ws")
+        val ast = BFParser.parseFile(reader)
+        println(ast)
+        BFEvaluator.evaluate(ast)
+
         // println(BFParser.parseLine("   \t\n"))
     }
 }
