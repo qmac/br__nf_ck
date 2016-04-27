5a4286d53617d867d9a79d3b8ee465aa3873188b
Eugene Ng <eugenen@utexas.edu>
2016-04-26 23:56:29 -0500

    fixed spacing

--- src/Evaluator.scala
+++ src/Evaluator.scala
@@ -1,7 +1,7 @@
 import scala.collection.mutable.Stack
 
 object BFEvaluator {
-	var stack = Stack[Int]()
+    var stack = Stack[Int]()
 
     def evaluate(operations: Any) : Unit = {
         operations match {
@@ -12,13 +12,13 @@ object BFEvaluator {
     def push(n: Int) = stack.push(n)
     def duplicate = stack.push(stack.top)
     def swap = {
-    	val prevTop = stack.pop
-    	val prevNext = stack.pop
-    	stack.push(prevTop)
-    	stack.push(prevNext)
+        val prevTop = stack.pop
+        val prevNext = stack.pop
+        stack.push(prevTop)
+        stack.push(prevNext)
     }
     def discard = stack.pop
     def execute(command: Any) : Unit = {
-    	
+        
     }
 }
