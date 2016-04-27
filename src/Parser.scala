f86924421af9fd4fc30cbc2d604e9f35bf177ee0
qmac <quinten.mcnamara@gmail.com>
2016-04-26 22:12:08 -0500

    converting binary to decimal integers

--- src/Parser.scala
+++ src/Parser.scala
@@ -1,6 +1,14 @@
 import scala.util.parsing.combinator._
+import java.lang.Integer
 
 object BFParser extends JavaTokenParsers {
+    def binary2Decimal(binary: List[Char]) : Int = {
+        val sign : Int = if (binary.head == '0') 1 else -1
+        val numberString : String = binary.tail.mkString
+        
+        return Integer.parseInt(numberString, 2) * sign
+    }
+
     def parseFile(in: java.io.Reader) = parseAll(prog, in).get
     def parseLine(line: String) = parseAll(prog, line).get
 
@@ -38,11 +46,11 @@ object BFParser extends JavaTokenParsers {
     def flow     : Parser[Any] = marklabel | callsubrt | jump | jumpzero | jumpneg | endsubrt | end
     def io       : Parser[Any] = outputchr | outputnum | readchar | readnum
 
-    def digit    : Parser[Any] = (space | tab)                      ^^ {case " " => 0 
-                                                                        case "\t" => 1}
-    def number   : Parser[Any] = (digit).* <~ lf
+    def digit    : Parser[Char] = (space | tab)                      ^^ {case " " => '0' 
+                                                                        case "\t" => '1'}
+    def number   : Parser[List[Char]] = (digit).+ <~ lf
     
-    def push     : Parser[Any] = space ~ space ~ number             ^^ {case a ~ b ~ n => "push(" + n + ")"}
+    def push     : Parser[Any] = space ~ space ~ number             ^^ {case a ~ b ~ n => "push(" + binary2Decimal(n) + ")"}
     def duplicate: Parser[Any] = space ~ lf ~ space                ^^^ "duplicate"
     def swap     : Parser[Any] = space ~ lf ~ tab                  ^^^ "swap"
     def discard  : Parser[Any] = space ~ lf ~ lf                   ^^^ "discard"
@@ -56,11 +64,11 @@ object BFParser extends JavaTokenParsers {
     def heapstore: Parser[Any] = tab ~ tab ~ space                 ^^^ "heapstore"
     def heapretrv: Parser[Any] = tab ~ tab ~ tab                   ^^^ "heapretrv"
     
-    def marklabel: Parser[Any] = lf ~ space ~ space ~ number        ^^ {case a ~ b ~ c ~ n => "mark(" + n + ")"}
-    def callsubrt: Parser[Any] = lf ~ space ~ tab ~ number          ^^ {case a ~ b ~ c ~ n => "call(" + n + ")"}
-    def jump     : Parser[Any] = lf ~ space ~ lf ~ number           ^^ {case a ~ b ~ c ~ n => "jump(" + n + ")"}
-    def jumpzero : Parser[Any] = lf ~ tab ~ space ~ number          ^^ {case a ~ b ~ c ~ n => "jumpzero(" + n + ")"}
-    def jumpneg  : Parser[Any] = lf ~ tab ~ tab ~ number            ^^ {case a ~ b ~ c ~ n => "jumpneg(" + n + ")"}
+    def marklabel: Parser[Any] = lf ~ space ~ space ~ number        ^^ {case a ~ b ~ c ~ n => "mark(" + binary2Decimal(n) + ")"}
+    def callsubrt: Parser[Any] = lf ~ space ~ tab ~ number          ^^ {case a ~ b ~ c ~ n => "call(" + binary2Decimal(n) + ")"}
+    def jump     : Parser[Any] = lf ~ space ~ lf ~ number           ^^ {case a ~ b ~ c ~ n => "jump(" + binary2Decimal(n) + ")"}
+    def jumpzero : Parser[Any] = lf ~ tab ~ space ~ number          ^^ {case a ~ b ~ c ~ n => "jumpzero(" + binary2Decimal(n) + ")"}
+    def jumpneg  : Parser[Any] = lf ~ tab ~ tab ~ number            ^^ {case a ~ b ~ c ~ n => "jumpneg(" + binary2Decimal(n) + ")"}
     def endsubrt : Parser[Any] = lf ~ tab ~ lf                     ^^^ "endsubrt"
     def end      : Parser[Any] = lf ~ lf ~ lf                      ^^^ "end"
     
